ed72400427ddfca6a6c941dc7dc360680c66853c
ChalleStr <chsr19@LAPTOP-OQQFOB7Q.localdomain>
2021-01-20 17:17:47 +0100

    Implemented timestamp.

--- index.js
+++ index.js
@@ -10,36 +10,27 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 /**
  * NEDAN FUNGERAR LOKALT!
  */
-// const io = require('socket.io')(server, {
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true,
+        allowedHeaders: {"Access-Control-Allow-Origin": "http://localhost:3000"}
+
+    }
+});
+/**
+ * Driftsatt läge.
+ */
+// const io = require("socket.io")(server, {
 //     cors: {
-//         //origin: "http://localhost:3000",
-//         origin: "https://charlottestrand.me:443",
+//         origin: "https://charlottestrand.me",
 //         methods: ["GET", "POST"],
-//         credentials: true,
-//         //allowedHeaders: {"Access-Control-Allow-Origin": "http://localhost:3000"}
-//         allowedHeaders: {"Access-Control-Allow-Origin": "https://charlottestrand.me:443"}
+//         allowedHeaders: ["Content-Type", "authorization", "Access-Control-Allow-Origin"],
+//         credentials: true
 //     }
 // });
 
-const io = require("socket.io")(server, {
-    cors: {
-        origin: "https://charlottestrand.me",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["Content-Type", "authorization", "Access-Control-Allow-Origin"],
-        credentials: true
-    }
-});
-
-const time = new Intl.DateTimeFormat("sv", {
-    timeStyle: "short",
-    timeZone: "Europe/Stockholm",
-});
-
-
-
-
-//app.use(cors());
-//app.use(router);
 
 app.get("/", (req, res, next) => {
     res.json({msg: "This is CORS-enabled for all origins."})
@@ -47,7 +38,6 @@ app.get("/", (req, res, next) => {
 
 io.on('connect', (socket) => {
     console.log("connected");
-    const timeStamp = `${time.format(Date.now())}`;
     socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
@@ -64,24 +54,24 @@ io.on('connect', (socket) => {
     });
 
     socket.on('sendMessage', (message, callback) => {
-    const user = getUser(socket.id);
+        const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', { user: user.name, text: message, timeStamp: timeStamp });
+        io.to(user.room).emit('message', { user: user.name, text: message, });
 
-    callback();
-  });
+        //if (callback) return callback();
+    });
 
-  socket.on('disconnect', () => {
-    const user = removeUser(socket.id);
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
 
-    if(user) {
-      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-    }
-  })
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
+    });
 });
 
-//server.listen(process.env.PORT || 8300, () => console.log(`Server listens at 8300.`));
+
 server.listen(8300, () => {
     console.log("Server listening on 8300");
 });
